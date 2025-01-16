8a79a2a434670381e9392cb41a49903895ab5c6b
W1ldr <dewillribeiro@gmail.com>
2025-01-16 10:30:31 +0100

    feat: add prepend, to_a, empty?, size, each, and display methods to doubly linked list

--- lib/data_types/doubly_linked_list/doubly_linked_list.rb
+++ lib/data_types/doubly_linked_list/doubly_linked_list.rb
@@ -22,6 +22,19 @@ module DataTypes
       end
     end
 
+    def prepend(value)
+      new_node = DoublyLinkedNode.new(value)
+      if @head.nil?
+        @head = new_node
+        @tail = new_node
+      else
+        new_node.next = @head
+        @head.previous = new_node
+        @head = new_node
+      end
+      new_node
+    end
+
     def find(value)
       current = @head
       while current
@@ -31,5 +44,35 @@ module DataTypes
       nil
     end
 
+    def to_a
+      arr = []
+      current = @head
+      while current
+        arr << current.value
+        current = current.next
+      end
+      arr
+    end
+
+    def empty?
+      @head.nil?
+    end
+
+    def size
+      count
+    end
+
+    def each
+      current = @head
+      while current
+      yield current.value
+      current = current.next
+      end
+    end
+
+    # Print the list elements
+    def display
+      each { |value| puts value }
+    end
   end
 end
