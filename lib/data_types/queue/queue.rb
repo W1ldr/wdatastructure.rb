c3e567789ee5a4cf719305799c939b704b3085e3
W1ldr <dewillribeiro@gmail.com>
2025-01-22 10:32:31 +0100

    feat: implement Queue class with enqueue, dequeue, peek, empty?, size, each, display, and to_a methods

---
+++ lib/data_types/queue/queue.rb
@@ -0,0 +1,42 @@
+# ruby
+module DataTypes
+  class Queue
+    include Enumerable
+
+    def initialize
+      @data = []
+    end
+
+    def enqueue(value)
+      @data.push(value)
+    end
+
+    def dequeue
+      @data.shift
+    end
+
+    def peek
+      @data.first
+    end
+
+    def empty?
+      @data.empty?
+    end
+
+    def size
+      @data.size
+    end
+
+    def each(&block)
+      @data.each(&block)
+    end
+
+    def display
+      each { |value| puts value }
+    end
+
+    def to_a
+      @data.dup
+    end
+  end
+end
